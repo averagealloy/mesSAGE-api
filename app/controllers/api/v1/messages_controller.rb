30ed7bae560ae2f691a85365d83e972b2b2f7feb
averagealloy <m.mctighe10@gmail.com>
2020-02-19 13:23:12 -0500

    messages controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,38 @@
 class Api::V1::MessagesController < ApplicationController
     def index 
-        @messages = Message.all 
+        messages = Message.all 
 
-        render json: @messages, status: 200
+        render json: messages, status: 200
+    end
+
+    def show
+        message = Message.find(params[:id])
+
+        render json: @message, status: 200
+    end 
+
+    def create
+        message = Message.new(message_params)
+        message.save
+        render json: message, status: 200
+
+    end
+
+    def update
+        message = Message.find(params[:id])
+        message.update(message_params)
+        render json: message, status: 200
+    end 
+
+    def destroy
+        message = Message.find(params[:id])
+        message.delete
+        render json: {messageId: message.id}
+    end 
+
+    private 
+
+    def message_params 
+        params.require(:message).permit(:message)
     end
 end
