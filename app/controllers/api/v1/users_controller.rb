8fa53d7369bc3ba1f9172982d8c701da5df48fd6
averagealloy <m.mctighe10@gmail.com>
2020-02-20 14:54:07 -0500

    users are finally showing up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,33 +1,32 @@
 class Api::V1::UsersController < ApplicationController
     def index 
-        @Users = User.all 
-
-        render json: @users, status: 200
+      users = User.all
+      render json: users, status: 200
     end
 
     def show
-        @user = User.find(params[:id])
+        user = User.find(params[:id])
 
-        render json: @user, status: 200
+        render json: user, status: 200
     end 
 
     def create
-        @user = User.create(user_params)
-        
-        render json: @user, status: 200
+        user = User.new(user_params)
+        user.save
+        render json: user, status: 200
 
     end
 
     def update
-        @user = User.find(params[:id])
-        @user.update(user_params)
-        render json: @user, status: 200
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: comment, status: 200
     end 
 
     def destroy
-        @user = User.find(params[:id])
-        @user.delete
-        render json: {message: "Thank you for using message"}
+        user = User.find(params[:id])
+        user.delete
+        render json: {message: "la8er nerd"}
     end 
 
     private 
